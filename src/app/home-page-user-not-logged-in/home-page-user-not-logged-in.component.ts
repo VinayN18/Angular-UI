1a33ba49a3a95ffaf7ca48ee097715eae8191c1e
Vinay Nakkina <vinay.1@iitj.ac.in>
2023-05-25 12:20:57 +0530

    Implemented text changing based on screen width

--- src/app/home-page-user-not-logged-in/home-page-user-not-logged-in.component.ts
+++ src/app/home-page-user-not-logged-in/home-page-user-not-logged-in.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home-page-user-not-logged-in',
   templateUrl: './home-page-user-not-logged-in.component.html',
   styleUrls: ['./home-page-user-not-logged-in.component.css'],
 })
-export class HomePageUserNotLoggedInComponent {
+export class HomePageUserNotLoggedInComponent implements OnInit {
   displaySignUpPage = 'none';
   displayLoginPage = 'none';
   displayStyle = 'none';
@@ -13,6 +13,18 @@ export class HomePageUserNotLoggedInComponent {
   hide2 = true;
   grp = 'Join Group';
   cls = 'box-leave';
+  allPostText = 'All Posts(32)';
+  postText = 'Write a Post';
+  share = '';
+  share2 = '';
+  share3 = '';
+  share4 = '';
+  signInTerms = '';
+  welcomeText = 'Sign In';
+
+  ngOnInit() {
+    this.updateParagraphText();
+  }
 
   onLoginPage() {
     if (this.grp === 'Join Group') {
@@ -38,5 +50,34 @@ export class HomePageUserNotLoggedInComponent {
     this.grp = 'Leave Group';
     this.displaySignUpPage = 'none';
     this.displayLoginPage = 'none';
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize(event: any) {
+    this.updateParagraphText();
+  }
+
+  updateParagraphText() {
+    const screenWidth = window.innerWidth;
+    if (screenWidth < 700) {
+      this.postText = 'Filter: All';
+      this.share = 'Share';
+      this.share2 = 'Share';
+      this.share3 = 'Share';
+      this.share4 = 'Share';
+      this.allPostText = 'Posts(368)';
+      this.signInTerms =
+        'By signing up, you agree to our Terms & conditions, Privacy policy';
+      this.welcomeText = 'Welcome back!';
+    } else {
+      this.postText = 'Write a Post';
+      this.share = '';
+      this.share2 = '';
+      this.share3 = '';
+      this.share4 = '';
+      this.allPostText = 'All Posts(32)';
+      this.signInTerms = '';
+      this.welcomeText = 'Sign In';
+    }
   }
 }
